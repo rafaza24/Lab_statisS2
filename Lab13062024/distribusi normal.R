4c7eb90d12b84ffacde46c779188813f50bd391d
RAFID FARHAN ZAI <154486040+rafaza24@users.noreply.github.com>
2024-06-13 14:21:25 +0700

    Add files via upload

--- Lab13062024/distribusi normal.R
+++ Lab13062024/distribusi normal.R
@@ -17,3 +17,12 @@ pnorm(45,mean = 30, sd = 25) -  pnorm(20,mean = 30, sd = 25)
 pnorm(52,mean = 39, sd = 21) -  pnorm(31,mean = 39, sd = 21)
 #3
 pnorm(42, mean = 39 ,sd = 21)
+
+
+#distribusi populasi
+x <- c(2,3,4,1,5,2,5,2,3,5,2,3,5,4,3,2,1,3,4,3,3,3,2,3,1,4,3,4)
+data <- table(x)/length(x)
+data1 <- round(data,digits = 3)
+data1
+barplot(data1)
+
